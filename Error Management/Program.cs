6ae7b6e7c2e56b94562db6ea4cd768fc9b6307b5
Ayse Oruccu <oruccu.ayse1999@gmail.com>
2024-09-24 10:42:17 +0300

    try catch add

---
+++ Error Management/Program.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+    Console.WriteLine("Bir sayı giriniz.");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Girmiş olduğunuz sayi: " + number);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Hata: " + ex.Message.ToString());
+    throw;
+}
+finally
+{
+    Console.WriteLine("İşlem tamamlandı.");
+}
+
+
+
+try
+{
+    int a = int.Parse(null);
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine("Boş değer girdiniz");
+    Console.WriteLine(ex);
+}
